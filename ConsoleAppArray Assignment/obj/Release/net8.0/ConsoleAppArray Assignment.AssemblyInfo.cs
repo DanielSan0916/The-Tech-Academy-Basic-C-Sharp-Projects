97e4aef7828a635879d2671aa477a5eee704fb22
DanielSan0916 <jessan0916@gmail.com>
2025-10-11 18:03:30 -0600

    Created 6 part console app

--- ConsoleAppArray Assignment/obj/Release/net8.0/ConsoleAppArray Assignment.AssemblyInfo.cs
+++ ConsoleAppArray Assignment/obj/Release/net8.0/ConsoleAppArray Assignment.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleAppArray Assignment")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3750ae4b0aabb5b6ee5d3ed5529d65f6c65e1b66")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleAppArray Assignment")]
